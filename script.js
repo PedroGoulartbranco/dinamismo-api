c692818908427820375a0169bfd593acdc2d86b9
Pedro Goulart Branco <pgoulartbranco@gmail.com>
2026-06-03 15:23:06 -0300

    começando o editar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let lista_post = []
 
 let div_users = document.getElementById("mostrar_users")
 let div_post = document.getElementById("mostrar_post")
+const modal_post = document.getElementById("janela-editar-post");
 
 fetch('https://jsonplaceholder.typicode.com/posts?utm_source=chatgpt.com')
 
@@ -27,7 +28,7 @@ fetch('https://jsonplaceholder.typicode.com/users?utm_source=chatgpt.com')
 
     .catch(error => console.log(error));
 
-function mostrarPost() {
+function mostrarPost() { //POST
     div_post.innerHTML = "";
     lista_post.forEach(usuario => {
     div_post.innerHTML += `
@@ -35,6 +36,7 @@ function mostrarPost() {
             <h3>${usuario.title} </h3>
             <p>ID: ${usuario.id}<br>ID Usuario: ${usuario.userId}<br>Descrição: ${usuario.body}</p>
             <button onclick="deletarPost(${usuario.id})">Deletar</button>
+            <button onclick="editarPost(${usuario.id})">Editar</button>
         </div>
     `
 });
@@ -49,6 +51,7 @@ function mostrarUsers() {
                 <p><strong>Username:</strong> ${usuario.username}</p>
                 <p><strong>E-mail:</strong> ${usuario.email}</p>
                 <p><strong>Company:</strong> ${usuario.company.name}</p>
+                <button onclick="deletarUsers(${usuario.id})">Deletar</button>
             </div>
         `;
     })
@@ -57,4 +60,23 @@ function mostrarUsers() {
 function deletarPost(id) {
     lista_post = lista_post.filter(post => post.id !== id);
     mostrarPost()
+}
+
+function deletarUsers(id) {
+    lista_users = lista_users.filter(user => user.id !== id)
+    mostrarUsers()
+}
+
+function editarPost(id) {
+    modal_post.style.display = "block"; 
+    
+    console.log("Editando o post:", idDoPost);
+}
+
+function fechar_janela() {
+    modal_post.style.display = "none"; 
+}
+
+function salvar_post(id, titulo, descricao) {
+    
 }
