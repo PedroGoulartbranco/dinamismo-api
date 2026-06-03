76ea01685a630dbddf7700bd68958b8a7dbc7c89
Pedro Goulart Branco <pgoulartbranco@gmail.com>
2026-06-03 14:52:48 -0300

    terminado o fetch e mostrar no html

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
+let lista_users = []
+let lista_post = []
+
+let div_users = document.getElementById("mostrar_users")
+let div_post = document.getElementById("mostrar_post")
+
 fetch('https://jsonplaceholder.typicode.com/posts?utm_source=chatgpt.com')
 
     .then(response => response.json())
 
-    .then(data => console.log(data))
+    .then(data => {
+         lista_post = data
+        mostrarPost()
+    })
 
     .catch(error => console.log(error));
+
+
+fetch('https://jsonplaceholder.typicode.com/users?utm_source=chatgpt.com')
+
+    .then(response => response.json())
+
+    .then(data => {
+        lista_users = data
+        mostrarUsers()
+    })
+
+    .catch(error => console.log(error));
+
+function mostrarPost() {
+    div_post.innerHTML = "";
+    lista_post.forEach(usuario => {
+    div_post.innerHTML += `
+        <div>
+            <h3>${usuario.title} </h3>
+            <p>ID: ${usuario.id}<br>ID Usuario: ${usuario.userId}<br>Descrição: ${usuario.body}</p>
+        </div>
+    `
+});
+}
+
+function mostrarUsers() {
+    div_users.innerHTML = ``
+    lista_users.forEach(usuario => {
+        div_users.innerHTML += `
+            <div class="user-card">
+                <h3>${usuario.name}</h3>
+                <p><strong>Username:</strong> ${usuario.username}</p>
+                <p><strong>E-mail:</strong> ${usuario.email}</p>
+                <p><strong>Company:</strong> ${usuario.company.name}</p>
+            </div>
+        `;
+    })
+}
